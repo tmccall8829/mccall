b2923c538130c79dc882b4ceddce6defd653e7c1
Tom McCall <thomas.e.mccall@gmail.com>
2022-09-11 18:57:09 -0700

    better style for global footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,9 @@
-import styles from './footer.module.css'
-
 export default function Footer() {
     return (
         <div>
-            <footer className={styles.footer}>
-                <a href='/'>Home</a>
+            <footer>
+                <a href='/'>Home 🏠</a>
+                <a href='/playground'>Playground 🎡</a>
             </footer>
         </div>
     )
