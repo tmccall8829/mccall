fdc0f1649018a4fc6e53cb81e71f4c6f0f8c4e70
Tom McCall <thomas.e.mccall@gmail.com>
2022-09-11 19:15:42 -0700

    add footer to layout for all basic pages

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,11 @@
 import styles from './layout.module.css'
+import Footer from '../components/footer'
 
 export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>;
+    return (
+        <div className={styles.container}>
+            {children}
+            <Footer />
+        </div>
+    )
 }
