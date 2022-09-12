cfa6b9e54e44e8729b0d9e7cb22e047806d98abb
Tom McCall <thomas.e.mccall@gmail.com>
2022-09-11 19:08:45 -0700

    remove duped .grid and separate footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
+import styles from './footer.module.css'
+
 export default function Footer() {
     return (
         <div>
-            <footer>
+            <footer className={styles.footer}>
                 <a href='/'>Home 🏠</a>
                 <a href='/playground'>Playground 🎡</a>
             </footer>
